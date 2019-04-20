dc6f21c9d99876d6a84433c26a2c8cf562d565f7
byh <734004037@qq.com>
2019-04-20 13:40:39 +0800

    day02_2

---
+++ huawei/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("#GroupWebsite_onclick").click(function () {
+        $("#GroupWebsite").slideToggle();
+        $("#GroupWebsite_onclick i").toggleClass("icon-arrow-up-s-line");
+    });
+});
